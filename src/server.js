60d8e02b3d9050bdf594a05b47da5b858e556ba4
Seiichi Okumiya <occmee@gmail.com>
2017-01-18 04:33:47 +0900

    init

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import koa from 'koa'; // koa@2
+import koaRouter from 'koa-router'; // koa-router@next
+import convert from 'koa-convert';
+import graphqlHTTP from 'koa-graphql';
+import { schema } from './schema';
+
+const app = new koa();
+const router = new koaRouter();
+const PORT = 3000;
+
+router.all('/graphql', convert(graphqlHTTP({
+  schema,
+  graphiql: true
+})));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(PORT);
